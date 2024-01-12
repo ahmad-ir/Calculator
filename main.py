476bf3f7b4480322e5f3bb1936369059c4ded554
Ahmad <amiriahmadir@gmail.com>
2024-01-12 14:58:39 +0100

    More methods added

--- main.py
+++ main.py
@@ -11,6 +11,27 @@ class Calculator():
        master.resizable(False, False)
        master.config(bg = 'gray')
 
+       self.equation = StringVar()
+       self.entry_value = ''
+       entry = Entry(
+           width =17, bg='#fff', 
+           font= ('Arial Bold', 28),
+           textvariable=self.equation)
+       entry.place(x=0, y=0)
+
+    def show(self, value):
+        self.entry_value += str(value)
+        self.equation.set(self.entry_value)
+    
+    def clear(self):
+        self.entry_value = ''
+        self.equation.set(self.entry_value)
+
+    def solve(self):
+        result = eval(self.entry_value)
+        self.equation.set(result)
+
+
 
 root = Tk()
 calculator = Calculator(root)
